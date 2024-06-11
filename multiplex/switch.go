2a587928d385afbb6aa628168afa949ad98577bf
ice-midas <170405428+ice-midas@users.noreply.github.com>
2024-06-11 14:17:42 +0200

    feat(multiplex): add multiplex replication for BlockExecutor, Indexers and Switch

---
+++ multiplex/switch.go
@@ -0,0 +1,29 @@
+package multiplex
+
+import (
+	"github.com/cometbft/cometbft/config"
+	"github.com/cometbft/cometbft/p2p"
+)
+
+type ScopedSwitch struct {
+	ScopeHash string
+	*p2p.Switch
+}
+
+type SwitchMultiplex []*ScopedSwitch
+
+// NewSwitch creates a new Switch with the given config.
+func NewScopedSwitch(
+	cfg *config.P2PConfig,
+	transport p2p.Transport,
+	userScopeHash string,
+	options ...p2p.SwitchOption,
+) *ScopedSwitch {
+	sw := p2p.NewSwitch(cfg, transport, options...)
+	ssw := &ScopedSwitch{
+		ScopeHash: userScopeHash,
+		Switch:    sw,
+	}
+
+	return ssw
+}
