c92ebab3adb361d23ef7e81cdce2770fdb62b8a7
ice-midas <170405428+ice-midas@users.noreply.github.com>
2024-08-22 11:37:28 +0200

    refactor(config): split functionality of EnsureRoot into EnsureFilesystem and EnsureConfig

--- config/toml.go
+++ config/toml.go
@@ -30,29 +30,64 @@ func init() {
 
 // ****** these are for production settings *********** //
 
-// EnsureRoot creates the root, config, and data directories if they don't exist,
-// and panics if it fails.
-func EnsureRoot(rootDir string) {
+// EnsureFilesystem creates the root, config and data directories if they don't exist.
+//
+// This method returns an error if any of the operations fail.
+//
+// This method is used when EnsureRoot is called, to setup the filesystem.
+func EnsureFilesystem(rootDir string) error {
 	if err := cmtos.EnsureDir(rootDir, DefaultDirPerm); err != nil {
-		panic(err.Error())
+		return err
 	}
 	if err := cmtos.EnsureDir(filepath.Join(rootDir, DefaultConfigDir), DefaultDirPerm); err != nil {
-		panic(err.Error())
+		return err
 	}
 	if err := cmtos.EnsureDir(filepath.Join(rootDir, DefaultDataDir), DefaultDirPerm); err != nil {
-		panic(err.Error())
+		return err
 	}
 
+	return nil
+}
+
+// EnsureConfig creates the default configuration file using a [Config] instance.
+//
+// This method is used when EnsureRoot is called, to setup default configuration.
+func EnsureConfig(rootDir string, conf *Config) {
 	configFilePath := filepath.Join(rootDir, defaultConfigFilePath)
 
 	// Write default config file if missing.
 	if !cmtos.FileExists(configFilePath) {
-		writeDefaultConfigFile(configFilePath)
+		WriteConfigFile(configFilePath, conf)
 	}
+}
+
+// EnsureConfigFile creates a "config.toml" file using a root directory and [Config] instance.
+//
+// This method is used to create separate configuration files per replicated chain (multiplex).
+func EnsureConfigFile(rootDir string, conf *Config) {
+	configFilePath := filepath.Join(rootDir, "config.toml")
+
+	// Write scoped config file if missing.
+	if !cmtos.FileExists(configFilePath) {
+		WriteConfigFile(configFilePath, conf)
+	}
+}
+
+// EnsureRoot creates the filesystem structure and necessary configuration files
+// or panics if it fails.
+//
+// Updated to execute separate filesystem and configuration existence checks.
+func EnsureRoot(rootDir string) {
+	if err := EnsureFilesystem(rootDir); err != nil {
+		panic(err.Error())
+	}
+
+	EnsureConfig(rootDir, DefaultConfig())
 }
 
 // EnsureRootMultiplex creates the scoped data directories if they don't exist,
 // and panics if it fails.
+//
 // FIXME: should use ScopeRegistry implementation and moved to multiplex/
 func EnsureRootMultiplex(rootDir string, config *BaseConfig) {
 	// Storage is located in scopes subfolders per each user
